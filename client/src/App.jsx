c642ac1de88b9f7e2beb2b7f952ad95144bb3ad7
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-06-11 12:34:59 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import "./index.css";
+import Chat from './Chat'
+
+const App = () => <Chat />;
+
+ReactDOM.render(<App />, document.getElementById("app"));
